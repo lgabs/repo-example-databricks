3752af3bb74e0f7bc500a95aef236c08899f3d50
Helena Porchia <lena.porchia@gmail.com>
2020-10-05 18:00:04 -0300

    remove coments

--- notebooks/test_my_class.py
+++ notebooks/test_my_class.py
@@ -1,27 +1,7 @@
-# Databricks notebook source
-# MAGIC %md 
-# MAGIC 
-# MAGIC - testar aqui no databricks seus notebooks
-# MAGIC - criar um repo no github
-# MAGIC - criar um token no github
-# MAGIC - add o token aqui no databricks
-# MAGIC - "linkar" um notebook ao repositório da sua conta com token adicionado
-# MAGIC - commitar em branch, abrir PR
-
-# COMMAND ----------
-
-# MAGIC %run ./my_class
-
-# COMMAND ----------
-
 my_cool_class = MyClass()
 
 my_cool_class
 
-# COMMAND ----------
-
 my_cool_class.f()
-
-# COMMAND ----------
 
 display(my_cool_class.query_sales())
