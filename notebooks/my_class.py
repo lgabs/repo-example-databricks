7f6c3c2de98c16cdceb11f0aaaef958ea3f5fbb6
Luan Fernandes <luangabriel70@gmail.com>
2021-02-08 11:48:52 -0300

    add my_class

---
+++ notebooks/my_class.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# https://docs.python.org/3/tutorial/classes.html
+
+class MyClass:
+    """
+    A simple example class
+    """
+    def f(self):
+        return 'hello world'
