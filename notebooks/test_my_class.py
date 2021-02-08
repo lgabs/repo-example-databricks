db8f7a9031fd04c840f02d41dd53bf851e8e5a8b
Luan Fernandes <luangabriel70@gmail.com>
2021-02-08 11:50:02 -0300

    add link to documentation

--- notebooks/test_my_class.py
+++ notebooks/test_my_class.py
@@ -7,6 +7,8 @@
 # MAGIC - add o token aqui no databricks
 # MAGIC - "linkar" um notebook ao repositório da sua conta com token adicionado
 # MAGIC - commitar em branch, abrir PR
+# MAGIC 
+# MAGIC documentação: https://docs.databricks.com/notebooks/github-version-control.html
 
 # COMMAND ----------
 
